91486ba6881368821d99d0d8791e4340e577aa21
BishrBinAli <bishrkc@gmail.com>
2023-01-31 13:34:42 +0000

    Create method read_rds_table() in DataExtractor class to read a sql table to pandas dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,22 @@
+import pandas as pd
+from database_utils import DatabaseConnector
+
+
 class DataExtractor:
-    pass
+    # def __init__() -> None:
+
+    def read_rds_table(self, DBConnector, table_name):
+        engine = DBConnector.db_engine
+        with engine.connect() as con:
+            df = pd.read_sql_table(table_name, con=con)
+        # print(df.info())
+        return df
+
+
+if __name__ == '__main__':
+    DBConnector = DatabaseConnector('db_creds.yaml')
+    DtExtractor = DataExtractor()
+    table_names = DBConnector.list_db_tables()
+    user_table_name = [table for table in table_names if 'user' in table][0]
+    print(user_table_name)
+    user_df = DtExtractor.read_rds_table(DBConnector, user_table_name)
